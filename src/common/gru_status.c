9d1727a5c437ddf9e9c91e50c5b69ca787a95e88
Otavio Rodolfo Piske <opiske@redhat.com>
2016-09-14 10:10:03 +0200

    REF: added basic status utility that will be needed for further code refactoring

---
+++ src/common/gru_status.c
@@ -0,0 +1,43 @@
+/**
+ Copyright 2016 Otavio Rodolfo Piske
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+#include "gru_status.h"
+
+void gru_status_set(gru_status_t *status, gru_status_code_t code, 
+        const char *message, ...) 
+{
+    va_list ap;
+    
+    if (status->message != NULL) {
+        gru_status_reset(status);
+    }
+    
+    if (message != NULL) {
+        va_start(ap, message);
+        vasprintf(&status->message, message, ap);
+        va_end(ap);
+    }
+    
+    status->code = code;
+}
+
+void gru_status_reset(gru_status_t *status) {
+    free(status->message);
+    status->message = NULL;
+}
+
+void gru_status_success(gru_status_t *status) {
+    gru_status_set(status, GRU_SUCCESS, NULL);
+}
---
+++ src/common/gru_status.h
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   gru_status.h
+ * Author: opiske
+ *
+ * Created on September 14, 2016, 10:03 AM
+ */
+
+#ifndef GRU_STATUS_H
+#define GRU_STATUS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/*
+ * Valid status codes
+ */
+typedef enum gru_status_code_t_ {
+    GRU_SUCCESS,
+    GRU_FAILURE,
+} gru_status_code_t;
+
+
+/**
+ * Status type
+ */
+typedef struct gru_status_t_ {
+    gru_status_code_t code;
+    char *message;
+} gru_status_t;
+
+/**
+ * Sets the status
+ * @param status an instance of the status object to set
+ * @param code the code to set the object to
+ * @param message the status message followed by any other parameter as accepted 
+ * by vasprintf
+ */
+void gru_status_set(gru_status_t *status, gru_status_code_t code, 
+                      const char *message, ...);
+
+/**
+ * Resets the status
+ * @param status an instance of the status object to set
+ */
+void gru_status_reset(gru_status_t *status);
+
+/**
+ * Sets the status to success
+ * @param status an instance of the status object to set
+ */
+void gru_status_success(gru_status_t *status);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GRU_STATUS_H */
+
