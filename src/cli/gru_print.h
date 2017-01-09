b7224d510b2f3d6d4dfc305bb5be6691711d68a5
Otavio Rodolfo Piske <opiske@redhat.com>
2017-01-09 20:12:08 +0100

    FIX: added missing files for new functionality

---
+++ src/cli/gru_print.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017 Otavio Piske <angusyoung@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * File:   gru_print.h
+ * Author: Otavio Piske <angusyoung@gmail.com>
+ *
+ * Created on January 9, 2017, 5:18 PM
+ */
+
+#ifndef GRU_PRINT_H
+#define GRU_PRINT_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "common/gru_colors.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void gru_print_err(const char *msg, ...);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GRU_PRINT_H */
+
