d07d4e3bf2585bbfff3aadfba222cd26a097c9a9
Otavio Rodolfo Piske <opiske@redhat.com>
2017-04-19 17:05:38 +0200

    FIX: test cases doing wrong microsecond conversion

--- test/time/gru_time_conversion_test.c
+++ test/time/gru_time_conversion_test.c
@@ -19,9 +19,36 @@
 
 #include "time/gru_time_utils.h"
 
-int test1() {
+/**
+ * Uses usec value that would cause the conversion to lost precision (ie: usec <= 999)
+ */
+int test_lost_precision_1() {
 	gru_timestamp_t ts = gru_time_read_str("1483228862.529");
-	uint64_t expected = 1483228862529;
+	uint64_t expected = 1483228862000; // All the microseconds <= 999 are lost
+
+	uint64_t ret = gru_time_to_milli(&ts);
+
+	if (ret != expected) {
+		printf("Returned value %" PRIu64 " does not mach the expected value %" PRIu64
+			   "\n",
+			ret,
+			expected);
+
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
+
+/**
+ * Uses usec value that would cause the conversion to lost some precision (ie: usec > 1000)
+ */
+int test_lost_precision_2() {
+	// 1999 = 1 millisecond
+	gru_timestamp_t ts = gru_time_read_str("1483228862.1001");
+	// All the microseconds thousandth range (ie.: 0 to 999 within eith 1000 units) are
+	// lost
+	uint64_t expected = 1483228862001;
 
 	uint64_t ret = gru_time_to_milli(&ts);
 
@@ -38,9 +65,10 @@ int test1() {
 }
 
 
-int test2() {
-	gru_timestamp_t ts = gru_time_read_str("1492582746.567");
-	uint64_t expected = 1492582746567;
+
+int test_lost_precision_3() {
+	gru_timestamp_t ts = gru_time_read_str("1492582746.9999998");
+	uint64_t expected = 1492582746999;
 
 	uint64_t ret = gru_time_to_milli(&ts);
 
@@ -57,8 +85,10 @@ int test2() {
 }
 
 int main(int argc, char **argv) {
-	if (test1() == EXIT_SUCCESS) {
-		return test2();
+	if (test_lost_precision_1() == EXIT_SUCCESS) {
+		if (test_lost_precision_2() == EXIT_SUCCESS) {
+			return test_lost_precision_3();
+		}
 	}
 
 	return EXIT_FAILURE;
