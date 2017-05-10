074c48bc2a398fb6b94aa411acd0f71c01eaea03
Otavio Rodolfo Piske <opiske@redhat.com>
2017-05-10 08:32:33 +0200

    NEW: build profile for building in release mode w/ Debug information

--- CMakeLists.default.cmake
+++ CMakeLists.default.cmake
@@ -9,7 +9,7 @@ add_custom_target(uninstall
 
 if (NOT CMAKE_BUILD_TYPE)
 	set(CMAKE_BUILD_TYPE Debug CACHE STRING
-      	"Choose the type of build, options are: Debug Release."
+      	"Choose the type of build, options are: Debug, RelWithDebInfo or Release."
       	FORCE
 	)
 endif(NOT CMAKE_BUILD_TYPE)
@@ -25,5 +25,9 @@ if (CMAKE_COMPILER_IS_GNUCXX)
 
 	set(CMAKE_C_FLAGS_RELEASE "-O2 -fomit-frame-pointer -D_FORTIFY_SOURCE=1 -fstack-protector-all ${CMAKE_USER_C_FLAGS}" CACHE STRING
 		"Flags used by the compiler during release build." FORCE
+	)
+
+	set(CMAKE_C_FLAGS_RELWITHDEBINFO "-g -O2 -D_FORTIFY_SOURCE=1 -fstack-protector-all ${CMAKE_USER_C_FLAGS}" CACHE STRING
+		"Flags used by the compiler during release build with debug information." FORCE
 	)
 endif (CMAKE_COMPILER_IS_GNUCXX)
