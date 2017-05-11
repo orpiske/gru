48137ba68c64ead3c5c53621866c52bfcd51f6cf
Otavio Rodolfo Piske <opiske@redhat.com>
2017-05-11 14:07:03 +0200

    NEW: file size conversion utilities

---
+++ src/common/gru_units.h
@@ -0,0 +1,53 @@
+/**
+ *    Copyright 2017 Otavio Rodolfo Piske <angusyoung@gmail.com>
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+#ifndef GRU_UNITS_H
+#define GRU_UNITS_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Convert an arbitrary value to kb
+ */
+static inline uint64_t gru_unit_kb(uint64_t num) {
+	return (num / 1024);
+}
+
+
+/**
+ * Convert an arbitrary value to mb
+ */
+static inline uint64_t gru_unit_mb(uint64_t num) {
+	return gru_unit_kb(num) / 1024;
+}
+
+
+/**
+ * Convert an arbitrary value to gb
+ */
+static inline uint64_t gru_unit_gb(uint64_t num) {
+	return gru_unit_mb(num) / 1024;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* GRU_UNITS_H */
---
+++ test/common/gru_units_test.c
@@ -0,0 +1,45 @@
+/**
+ *    Copyright 2017 Otavio Rodolfo Piske <angusyoung@gmail.com>
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "common/gru_units.h"
+
+
+int main(int argc, char **argv) {
+	const uint64_t bytes = 10048765952;
+	
+	if (gru_unit_kb(bytes) != 9813248) {
+		fprintf(stderr, "Failed to convert to kilobytes");
+
+		return EXIT_FAILURE;
+	}
+
+	if (gru_unit_mb(bytes) != 9583) {
+		fprintf(stderr, "Failed to convert to megabytes");
+
+		return EXIT_FAILURE;
+	}
+
+	if (gru_unit_gb(bytes) != 9) {
+		fprintf(stderr, "Failed to convert to gigabytes");
+
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
